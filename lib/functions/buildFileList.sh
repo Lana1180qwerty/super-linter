2c154d4b95e010ae8c644521f8ebb50d29ffdee2
Gabo <gabo.fdc@gmail.com>
2021-01-08 23:41:59 -0500

    Fix rebase errors

--- lib/functions/buildFileList.sh
+++ lib/functions/buildFileList.sh
@@ -349,8 +349,8 @@ function BuildFileList() {
       ################################
       FILE_ARRAY_JAVASCRIPT_ES+=("${FILE}")
       FILE_ARRAY_JAVASCRIPT_STANDARD+=("${FILE}")
+      FILE_ARRAY_JAVASCRIPT_PRETTIER+=("${FILE}")
       FILE_ARRAY_JSCPD+=("${FILE}")
-
 
     ######################
     # Get the JSON files #
@@ -362,6 +362,15 @@ function BuildFileList() {
       FILE_ARRAY_JSON+=("${FILE}")
       FILE_ARRAY_JSCPD+=("${FILE}")
 
+      ############################
+      # Check if file is Ansible #
+      ############################
+      if DetectAnsibleFile "${ANSIBLE_DIRECTORY}" "${FILE}"; then
+        ################################
+        # Append the file to the array #
+        ################################
+        FILE_ARRAY_ANSIBLE+=("${FILE}")
+      fi
       ############################
       # Check if file is OpenAPI #
       ############################
